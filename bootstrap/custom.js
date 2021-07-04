e4770b240c292a6c7fd38ef4a57634c3ff559f7f
mahmoudsalah1 <mahmoudsalah088@gmail.com>
2021-07-05 01:55:04 +0200

    first commit

---
+++ bootstrap/custom.js
@@ -0,0 +1,19 @@
+$( function() {
+    $( "#accordion" ).accordion({
+      collapsible: true
+    });
+  } );
+  function changedirection(obj){
+    var arrowdir = document.getElementsByClassName("fa-arrow-up")
+    
+
+  }
+
+  $('.fade').slick({
+    dots: true,
+    infinite: true,
+    speed: 500,
+    fade: true,
+    cssEase: 'linear'
+  });
+
